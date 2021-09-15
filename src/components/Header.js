37c28fe8fac8559025e07990f68d180b2ada893b
Fran Canete <francanetedev@gmail.com>
2021-09-15 12:08:50 +0100

    Add project details

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,48 @@
-import { FaCss3Alt } from 'react-icons/fa';
-import { FaHtml5 } from 'react-icons/fa';
-import { SiNpm } from 'react-icons/si';
-import { SiGit } from 'react-icons/si';
-import { SiJavascript } from 'react-icons/si';
-import { FaReact } from 'react-icons/fa';
+import { FaCss3Alt } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaNode } from "react-icons/fa";
+import { SiNpm } from "react-icons/si";
+import { SiGit } from "react-icons/si";
+import { SiJavascript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
 
 const Header = () => {
-  return ( 
+  return (
     <header className="header">
       <div className="hero-container">
-        <h1 className="headline">Hi, my name is <span className="h1Color">Fran Canete</span><br/> I'm a Frontend Developer</h1>
+        <h1 className="headline">
+          Hi, my name is <span className="h1Color">Fran Canete</span>
+          <br /> I'm a Frontend Developer
+        </h1>
         <div className="hero-icons">
-          <i className="fa-hero"><FaCss3Alt fill="#133245" /></i>
-          <i className="fa-hero"><FaHtml5 fill="#133245" /></i>
-          <i className="fa-hero"><SiNpm fill="#133245" /></i>
-          <i className="fa-hero"><SiGit fill="#133245" /></i>
-          <i className="fa-hero"><SiJavascript fill="#133245" /></i>
-          <i className="fa-hero"><FaReact fill="#133245" /></i>
-        </div>        
-        <button className="cta-button">Know more</button>
+          <i className="fa-hero">
+            <FaCss3Alt fill="#133245" />
+          </i>
+          <i className="fa-hero">
+            <FaHtml5 fill="#133245" />
+          </i>
+          <i className="fa-hero">
+            <SiNpm fill="#133245" />
+          </i>
+          <i className="fa-hero">
+            <FaNode fill="#133245" />
+          </i>
+          <i className="fa-hero">
+            <SiGit fill="#133245" />
+          </i>
+          <i className="fa-hero">
+            <SiJavascript fill="#133245" />
+          </i>
+          <i className="fa-hero">
+            <FaReact fill="#133245" />
+          </i>
+        </div>
+        <a href="#about">
+          <button className="cta-button">Know more</button>
+        </a>
       </div>
     </header>
-   );
-}
- 
+  );
+};
+
 export default Header;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,50 +1,103 @@
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub } from "react-icons/fa";
 
 const Projects = () => {
-  return ( 
-
+  return (
     <div>
       <div className="flex-container projects">
         <div className="card">
-          <h2 className="project-title">The Amazing Project #1</h2>
-          <p>I’m an aspiring web designer who loves everything about the web. I've lived in lots of different places and have worked in lots of different jobs. I’m excited to bring my life experience to the process of building fantastic looking websites.</p>
-          <p>I’ve been a professional cook and gardener and am a life-long learner who's always interested in expanding my skills.</p>
-          <button className="cta-button">See Live</button>
+          <h2 className="project-title">Cryptocurrency Tracker</h2>
+          <p>
+            I’m an aspiring web designer who loves everything about the web.
+            I've lived in lots of different places and have worked in lots of
+            different jobs. I’m excited to bring my life experience to the
+            process of building fantastic looking websites.
+          </p>
+          <p>
+            I’ve been a professional cook and gardener and am a life-long
+            learner who's always interested in expanding my skills.
+          </p>
+          <a
+            href="https://cryptotracker-fran.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="cta-button">See Live</button>
+          </a>
+
           <i className="fab fa-github fa-hero" id="second-bttn-icon">
-          <a href='https://www.youtube.com/watch?v=JUrcWu57itY' className="social"><FaGithub fill="black" /></a>
+            <a
+              href="https://github.com/francanete/crypto-tracker"
+              target="_blank"
+              rel="noreferrer"
+              className="social"
+            >
+              <FaGithub fill="black" />
+            </a>
           </i>
-        </div> 
+        </div>
         <div className="card">
-          <img src="./images/project.jpeg" alt="projects" id="img-project" />
-        </div> 
+          <img src="./images/project1.png" alt="projects" id="img-project" />
+        </div>
+      </div>
+      <div className="flex-container projects">
+        <div className="card">
+          <h2 className="project-title">Flickr Photo Gallery</h2>
+          <p>
+            I’m an aspiring web <strong>React.js</strong> designer who loves
+            everything about the web. I've lived in lots of different places and
+            have worked in lots of different jobs. I’m excited to bring my life
+            experience to the process of building fantastic looking websites.
+          </p>
+          <p>
+            I’ve been a professional cook and gardener and am a life-long
+            learner who's always interested in expanding my skills.
+          </p>
+          <a
+            href="https://cryptotracker-fran.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="cta-button">See Live</button>
+          </a>
+          {/* <button className="cta-button">See Live</button> */}
+          <i className="fab fa-github fa-hero" id="second-bttn-icon">
+            <a
+              href="https://github.com/francanete/photo-gallery-api"
+              target="_blank"
+              rel="noreferrer"
+              className="social"
+            >
+              <FaGithub fill="black" />
+            </a>
+          </i>
+          {/* <i className="fab fa-github fa-hero" id="second-bttn-icon"></i> */}
+        </div>
+        <div className="card">
+          <img src="./images/project2.png" alt="projects" id="img-project" />
+        </div>
       </div>
       <div className="flex-container projects">
         <div className="card">
           <h2 className="project-title">The Amazing Project #1</h2>
-          <p>I’m an aspiring web designer who loves everything about the web. I've lived in lots of different places and have worked in lots of different jobs. I’m excited to bring my life experience to the process of building fantastic looking websites.</p>
-          <p>I’ve been a professional cook and gardener and am a life-long learner who's always interested in expanding my skills.</p>
+          <p>
+            I’m an aspiring web designer who loves everything about the web.
+            I've lived in lots of different places and have worked in lots of
+            different jobs. I’m excited to bring my life experience to the
+            process of building fantastic looking websites.
+          </p>
+          <p>
+            I’ve been a professional cook and gardener and am a life-long
+            learner who's always interested in expanding my skills.
+          </p>
           <button className="cta-button">See Live</button>
           <i className="fab fa-github fa-hero" id="second-bttn-icon"></i>
-        </div> 
+        </div>
         <div className="card">
           <img src="./images/project.jpeg" alt="projects" id="img-project" />
-        </div> 
-      </div>
-      <div className="flex-container projects">
-        <div className="card">
-          <h2 className="project-title">The Amazing Project #1</h2>
-          <p>I’m an aspiring web designer who loves everything about the web. I've lived in lots of different places and have worked in lots of different jobs. I’m excited to bring my life experience to the process of building fantastic looking websites.</p>
-          <p>I’ve been a professional cook and gardener and am a life-long learner who's always interested in expanding my skills.</p>
-          <button className="cta-button">See Live</button>
-          <i className="fab fa-github fa-hero" id="second-bttn-icon"></i>
-        </div> 
-        <div className="card">
-          <img src="./images/project.jpeg" alt="projects" id="img-project" />
-        </div> 
+        </div>
       </div>
     </div>
-    
-   );
-}
- 
+  );
+};
+
 export default Projects;
