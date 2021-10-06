6343804ee3cf6cc170810c4753e0a4018ae4e815
Fran Canete <francanetedev@gmail.com>
2021-10-06 17:44:04 +0100

    Update project text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,14 +7,13 @@ const Projects = () => {
         <div className="card">
           <h2 className="project-title">Cryptocurrency Tracker</h2>
           <p>
-            I’m an aspiring web designer who loves everything about the web.
-            I've lived in lots of different places and have worked in lots of
-            different jobs. I’m excited to bring my life experience to the
-            process of building fantastic looking websites.
+            In this project, I am calling a public API using the Axios library
+            to get all the necessary data in the application. to get all the
+            necessary data in the application.
           </p>
           <p>
-            I’ve been a professional cook and gardener and am a life-long
-            learner who's always interested in expanding my skills.
+            The data can be updated after 60 seconds via the update button,
+            making a new API call and displaying the updated results.
           </p>
           <p>
             <small>React, React States, React Hooks, Fetch</small>
@@ -46,14 +45,16 @@ const Projects = () => {
         <div className="card">
           <h2 className="project-title">Flickr Photo Gallery</h2>
           <p>
-            I’m an aspiring web <strong>React.js</strong> designer who loves
-            everything about the web. I've lived in lots of different places and
-            have worked in lots of different jobs. I’m excited to bring my life
-            experience to the process of building fantastic looking websites.
+            This application connects to the Flickr API and accesses its
+            database of photos. The user can perform a search and get results
+            for a specific search term.
           </p>
           <p>
-            I’ve been a professional cook and gardener and am a life-long
-            learner who's always interested in expanding my skills.
+            I added React Route to display the searches and a "Not Found" page
+            to handle 404 errors.
+          </p>
+          <p>
+            <small>React, Custom Hooks, Fetch, React Router, REST API</small>
           </p>
           <a
             href="https://photo-gallery-flickr.netlify.app"
@@ -83,14 +84,16 @@ const Projects = () => {
         <div className="card">
           <h2 className="project-title">Star Wars Battle</h2>
           <p>
-            I’m an aspiring web designer who loves everything about the web.
-            I've lived in lots of different places and have worked in lots of
-            different jobs. I’m excited to bring my life experience to the
-            process of building fantastic looking websites.
+            Using the Star Wars API I added some logic to face 2 random
+            characters from the saga that will appear in battle! Whoever has the
+            tallest height wins.
           </p>
           <p>
-            I’ve been a professional cook and gardener and am a life-long
-            learner who's always interested in expanding my skills.
+            I built this project using React and Axios as an API library to get
+            all the necessary data. May the code be with you!
+          </p>
+          <p>
+            <small>React, React States, React Hooks, Fetch</small>
           </p>
           <a
             href="https://star-wars-battle.netlify.app"
